68a8bddb34d61d03f4e9ab919313f51704ad4097
shubhamrp1699 <73349951+shubhamrp1699@users.noreply.github.com>
2021-07-08 12:48:50 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-011c99152163a87ae"
+  instance_type = "t2.micro"
+  tags = {
+    Name             = "ec2-phpdemo"
+    Created_By       = "Terraform_Automation"
+    Application_Name = "Terraform_Demo"
+
+  }
+
+  key_name               = "ShubhamAWS"
+  subnet_id              = "subnet-90cc23fb"
+  
+
+  user_data = <<-EOF
+                #!/bin/bash 
+                yum update -y 
+                yum install wget 
+                wget https://www.apachefriends.org/xampp-files/7.0.23/xampp-linux-x64-7.0.23-0-installer.run 
+                chmod +x xampp-linux-x64-7.0.23-0-installer.run 
+                ./xampp-linux-x64-7.0.23-0-installer.run 
+                sudo /opt/lampp/lampp start
+                sudo /opt/lampp/lampp restart
+                EOF
+}
